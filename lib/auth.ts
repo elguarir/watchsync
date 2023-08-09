70bf44caa490b93ffe9c85d594f4ae38bbc843d9
Mohamed Elguarir <120427922+MohamedElguarir@users.noreply.github.com>
2023-08-09 10:41:58 +0000

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./db";
+import prisma from "./db";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db) as any,
+  adapter: PrismaAdapter(prisma) as any,
   session: {
     strategy: "jwt",
   },
